36b1f0592804bcbffc3bbbb0880f55e6eb069842
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-07 08:01:51 -0500

    Update back-to-top-block.php

--- back-to-top-block.php
+++ back-to-top-block.php
@@ -10,20 +10,13 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       back-to-top-block
  *
- * @package CreateBlock
+ * @package back-to-top-block
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_back_to_top_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
