d84370050a83dcec74f32f719c46c4e3537fb63a
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-17 21:20:37 -0500

    Version 1.0.1
    Always find something after a release

--- back-to-top-block.php
+++ back-to-top-block.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Plugin Name:       Back To Top Block
+ * Plugin URI:        https://github.com/philhoyt/Back-to-Top-Block
  * Description:       Adds a customizable Back to Top button that helps visitors return to the top of the page.
  * Requires at least: 6.6
  * Requires PHP:      7.2
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Phil Hoyt
+ * Author URI:        https://philhoyt.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       back-to-top-block
@@ -23,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * This function is called during WordPress initialization to register
  * the Back to Top block using the compiled assets from the build directory.
  *
- * @since 1.0.0
+ * @since 1.0.1
  * @return void
  */
 function back_to_top_block_init() {
