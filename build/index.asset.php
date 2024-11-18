d64d018921a7ffc8473f797112e46975bd2e5a33
Phil Hoyt <philhoyt@users.noreply.github.com>
2024-11-17 21:25:17 -0500

    1.0.1

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ebd681d481e3f1cc0f61');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0da12530bb844682af81');
